ee9724636814434ec6d9a481fa8794c92a3196bd
Chris <chrisnlott@gmail.com>
2019-01-11 16:05:50 -0600

    fix: bg:black -> bg-black

--- src/components/Settings/index.js
+++ src/components/Settings/index.js
@@ -7,7 +7,7 @@ import { Footer } from '../Footer';
 // Only used for admin panel
 export default ({ header, actionBar, footer }) => {
   return (
-    <div className="bg:black">
+    <div className="bg-black">
       <Header key="header" header={header} />
       <div style={{ height: 100 }} />
       <ActionBar key="actionBar" {...actionBar} />
